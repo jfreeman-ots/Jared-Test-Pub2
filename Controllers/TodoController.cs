63d3aafd8931e10e305764df2d9ca39adae7f125
p00204852a <p00204852a@AZ-MMMR-DVOPS02>
2023-11-27 15:10:46 -0600

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TodoApp01.Models;
+
+namespace TodoApp01.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static List<Todo> todos = new List<Todo>();
+
+        public IActionResult Index()
+        {
+            return View(todos);
+        }
+
+        [HttpPost]
+        public IActionResult AddTodo(string title)
+        {
+            var todo = new Todo { Title = title };
+            todos.Add(todo);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ToggleDone(int id)
+        {
+            var todo = todos.Find(t => t.Id == id);
+            if (todo != null)
+            {
+                todo.IsDone = !todo.IsDone;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp01.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
